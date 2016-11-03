5fd6bf46e183cfc131daea3ac85217c75ac530f3
Mungrel <npol807@aucklanduni.ac.nz>
2016-11-04 07:55:37 +1300

    Basic results frame layout done, added default avatar image

--- src/main/java/gui/ResultsFrame.java
+++ src/main/java/gui/ResultsFrame.java
@@ -7,10 +7,14 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class ResultsFrame extends JFrame {
 
 	private JPanel contentPane;
+	protected static ResultsFrame resultsFrame = new ResultsFrame();
 
 	/**
 	 * Launch the application.
@@ -19,8 +23,7 @@ public class ResultsFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ResultsFrame frame = new ResultsFrame();
-					frame.setVisible(true);
+					resultsFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -32,14 +35,32 @@ public class ResultsFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public ResultsFrame() {
+		setResizable(false);
 		setTitle("SteamQuery - Results");
 		setIconImage(new ImageIcon("src/main/resources/images/share_steam_logo.png").getImage());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 408, 433);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 80, 382, 313);
+		tabbedPane.setFocusable(false);
+		contentPane.add(tabbedPane);
+		
+		JPanel playerSummaryPanel = new JPanel();
+		tabbedPane.addTab("Summary", null, playerSummaryPanel, null);
+		tabbedPane.setEnabledAt(0, true);
+		
+		JLabel lblDisplayname = new JLabel("DisplayName");
+		lblDisplayname.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		lblDisplayname.setBounds(10, 11, 278, 64);
+		contentPane.add(lblDisplayname);
+		
+		JPanel avatarImagePanel = new JPanel();
+		avatarImagePanel.setBounds(328, 11, 64, 64);
+		contentPane.add(avatarImagePanel);
 	}
-
 }
