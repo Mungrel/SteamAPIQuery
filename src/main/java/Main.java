0e64dda3c08b1fe6b584156ee6c7eefcc5e7b288
Mungrel <npol807@aucklanduni.ac.nz>
2016-11-08 21:27:18 +1300

    Added Main class which starts application

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import frames.SearchFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SearchFrame.searchFrame.setLocationRelativeTo(null);
+		SearchFrame.searchFrame.setVisible(true);
+	}
+}
