d38fe4fa2fc6b5aad1fc08e6d234e80b4a6cc926
Mungrel <npol807@aucklanduni.ac.nz>
2016-11-13 19:47:41 +1300

    Further label adjustment

--- src/main/java/panels/SummaryPanel.java
+++ src/main/java/panels/SummaryPanel.java
@@ -14,47 +14,47 @@ public class SummaryPanel extends JPanel {
 		
 		JLabel lblUrl = new JLabel("URL");
 		lblUrl.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblUrl.setBounds(10, 11, 72, 14);
+		lblUrl.setBounds(10, 11, 96, 14);
 		add(lblUrl);
 		
 		JLabel lblLastOnline = new JLabel("Last Online");
 		lblLastOnline.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblLastOnline.setBounds(10, 36, 72, 14);
+		lblLastOnline.setBounds(20, 36, 86, 14);
 		add(lblLastOnline);
 		
 		JLabel lblRealName = new JLabel("Real Name");
 		lblRealName.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblRealName.setBounds(10, 61, 72, 14);
+		lblRealName.setBounds(10, 61, 96, 14);
 		add(lblRealName);
 		
 		JLabel lblMemberSince = new JLabel("Member Since");
 		lblMemberSince.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblMemberSince.setBounds(10, 86, 72, 14);
+		lblMemberSince.setBounds(10, 86, 96, 14);
 		add(lblMemberSince);
 		
 		JLabel lblCountry = new JLabel("Country");
 		lblCountry.setHorizontalAlignment(SwingConstants.TRAILING);
-		lblCountry.setBounds(10, 111, 72, 14);
+		lblCountry.setBounds(20, 111, 86, 14);
 		add(lblCountry);
 		
 		JLabel URLValueLabel = new JLabel(ps.getProfileURL());
-		URLValueLabel.setBounds(92, 11, 289, 14);
+		URLValueLabel.setBounds(162, 11, 289, 14);
 		add(URLValueLabel);
 		
 		JLabel lastOnlineValueLabel = new JLabel(""+ps.getLastLogOff());
-		lastOnlineValueLabel.setBounds(92, 36, 81, 14);
+		lastOnlineValueLabel.setBounds(162, 36, 81, 14);
 		add(lastOnlineValueLabel);
 		
 		JLabel realNameValueLabel = new JLabel(ps.getRealName());
-		realNameValueLabel.setBounds(92, 61, 81, 14);
+		realNameValueLabel.setBounds(162, 61, 81, 14);
 		add(realNameValueLabel);
 		
 		JLabel memberSinceValueLabel = new JLabel(""+ps.getTimeCreated());
-		memberSinceValueLabel.setBounds(92, 86, 81, 14);
+		memberSinceValueLabel.setBounds(162, 86, 81, 14);
 		add(memberSinceValueLabel);
 		
 		JLabel countryValueLabel = new JLabel(ps.getCountry());
-		countryValueLabel.setBounds(92, 111, 81, 14);
+		countryValueLabel.setBounds(162, 111, 81, 14);
 		add(countryValueLabel);
 
 	}
