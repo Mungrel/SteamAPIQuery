ec0430961f9d8a9e98b6527c423377f046bc187b
Mungrel <npol807@aucklanduni.ac.nz>
2016-12-06 21:04:16 +1300

    Friend panel now gets avatar image

--- src/main/java/panels/FriendStateContainer.java
+++ src/main/java/panels/FriendStateContainer.java
@@ -4,6 +4,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import domain.Friend;
+import domain.PlayerSummary;
+import query.QueryManager;
+import utils.ImageManager;
 
 @SuppressWarnings("serial")
 public class FriendStateContainer extends JPanel {
@@ -11,9 +14,14 @@ public class FriendStateContainer extends JPanel {
 	public FriendStateContainer(Friend friend) {
 		setLayout(null);
 		
+		PlayerSummary friendSummary = QueryManager.getPlayerSummary(friend.getSteam64ID());
+		
 		JLabel avatarImgLbl = new JLabel("");
 		avatarImgLbl.setBounds(10, 11, 64, 64);
+		avatarImgLbl.setIcon(ImageManager.createImageIcon(friendSummary.getAvatarImageURL()));
 		add(avatarImgLbl);
+		
+		//TODO: Assign relevant fields to FSC from PlayerSummary and Friend objects
 
 	}
 }
