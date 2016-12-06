d26a100b7df1ce17be0142d2dde846b15f6f0a5c
Mungrel <npol807@aucklanduni.ac.nz>
2016-12-06 21:00:04 +1300

    Added image label to FSC

--- src/main/java/panels/FriendStateContainer.java
+++ src/main/java/panels/FriendStateContainer.java
@@ -1,12 +1,19 @@
 package panels;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import domain.Friend;
 
 @SuppressWarnings("serial")
 public class FriendStateContainer extends JPanel {
 
-	public FriendStateContainer() {
+	public FriendStateContainer(Friend friend) {
+		setLayout(null);
+		
+		JLabel avatarImgLbl = new JLabel("");
+		avatarImgLbl.setBounds(10, 11, 64, 64);
+		add(avatarImgLbl);
 
 	}
-
 }
