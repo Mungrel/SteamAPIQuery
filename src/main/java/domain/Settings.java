89f78dd2a51fef7f4a05ac728627ba2e9f3554e3
Mungrel <npol807@aucklanduni.ac.nz>
2016-12-21 16:07:56 +1300

    Added Settings object for JSON state saving

---
+++ src/main/java/domain/Settings.java
@@ -0,0 +1,23 @@
+package domain;
+
+public class Settings {
+	
+	private boolean clearCacheStartup;
+	private boolean splashStartup;
+	
+	public boolean isClearCacheStartup() {
+		return clearCacheStartup;
+	}
+	
+	public void setClearCacheStartup(boolean clearCacheStartup) {
+		this.clearCacheStartup = clearCacheStartup;
+	}
+	
+	public boolean isSplashStartup() {
+		return splashStartup;
+	}
+	
+	public void setSplashStartup(boolean splashStartup) {
+		this.splashStartup = splashStartup;
+	}
+}
