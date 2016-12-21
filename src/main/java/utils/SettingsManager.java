603ac3272f858b57b9c5a21bd7feac9f6f058940
Mungrel <npol807@aucklanduni.ac.nz>
2016-12-21 16:10:08 +1300

    Added settingsManager skeleton for exporting/importing settings

---
+++ src/main/java/utils/SettingsManager.java
@@ -0,0 +1,16 @@
+package utils;
+
+import domain.Settings;
+
+public class SettingsManager {
+	
+	public Settings fromJSON(String json){
+		//TODO: implement this
+		return new Settings();
+	}
+	
+	public String toJSON(Settings settings){
+		//TODO: implement this
+		return null;
+	}
+}
