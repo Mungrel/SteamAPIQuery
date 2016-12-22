5f879c5012ca35356ab4a2ed89be2e454ba943d5
Mungrel <npol807@aucklanduni.ac.nz>
2016-12-23 12:55:37 +1300

    Added method for reading from settings file, comment outlining planned process

--- src/main/java/utils/SettingsManager.java
+++ src/main/java/utils/SettingsManager.java
@@ -1,10 +1,28 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import com.google.gson.Gson;
 
 import domain.Settings;
 
 public class SettingsManager {
+	
+	private static String settingsFilePath = "config";
+	
+	private String readSettings() throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(settingsFilePath));
+		
+		String json = "";
+		String line = null;
+		while((line = br.readLine()) != null){
+			json += line;
+		}
+		return json;
+	}
 
 	public Settings fromJSON(String json) {
 		Gson gson = new Gson();
@@ -16,3 +34,8 @@ public class SettingsManager {
 		return gson.toJson(settings);
 	}
 }
+
+// pull json string from settings file
+// marshall to Settings object
+// execute any settings, e.g. show splash
+// reflect saved state in options frame
