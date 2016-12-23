701f0811b24c7928ffa4f8e43242dd53676ac587
Mungrel <npol807@aucklanduni.ac.nz>
2016-12-23 13:19:03 +1300

    Moved non-startup routine method calls to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,16 +7,16 @@ import utils.SettingsManager;
 public class Main {
 
 	public static void main(String[] args) {
+		FileManager.checkAndBuildTmpDir();
 		startupRoutine();
+		SearchFrame.open();
 	}
 	
 	private static void startupRoutine(){
-		FileManager.checkAndBuildTmpDir();
 		Settings settings = SettingsManager.getSettings();
 		if (settings.isSplashStartup())
 			Splash.splash();
 		if (settings.isClearCacheStartup())
 			FileManager.emptyTmpDir();
-		SearchFrame.open();
 	}
 }
